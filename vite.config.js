9b702e4fd896fa46d6b11b2b77cd96e28d6b15d9
AKASH KUMAR VERMA <akverma2496@gmail.com>
2025-11-10 17:43:11 +0530

    redirect-added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true,           // ← allows external devices to connect
     port: 5173,           // ← default Vite port (can be changed)
